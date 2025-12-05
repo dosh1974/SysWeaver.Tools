7c9d927db01e048cd857c8b658291fc12ea383d3
SolidEq <dh@solidicon.com>
2025-12-05 15:09:56 +0100

    Clean script

--- Clean_All.bat
+++ Clean_All.bat
@@ -1,8 +1,12 @@
 @echo off
+if not "%1%"=="" goto skip1
 echo All projects will be cleaned is this folder!
 echo A full rebuild of everything will be required!
 echo Press Ctrl+C now to abort!
 pause
+:skip1
+
 cd /D %~dp0
 ..\SysWeaver\_tools\VsClean\VsClean.exe .
-pause
+
+if "%1%"=="" pause
